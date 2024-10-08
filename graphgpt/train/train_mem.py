7af4cf54506eb86df9d337ff1162eb8dc057cf61
superallen13 <hi@allenliu.xyz>
2024-10-08 16:55:42 +1000

    Auto set the graphgpt as a package.

--- graphgpt/train/train_mem.py
+++ graphgpt/train/train_mem.py
@@ -1,6 +1,8 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
 # Need to call this before importing transformers.
+import rootutils
+rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 from graphgpt.train.llama_flash_attn_monkey_patch import (
     replace_llama_attn_with_flash_attn,
 )
